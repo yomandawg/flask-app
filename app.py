14c9cbf477197f53bc5bb3ba50976d50a752321a
Youk Changsu <csyouk@gmail.com>
2020-03-15 21:37:39 +0900

    Add mongodb connect example
    Download mongodb at : https://www.mongodb.com/download-center/community
    Read tech document at : https://flask-pymongo.readthedocs.io/en/latest/
    Read difference between RDB and NoSQL : https://sjh836.tistory.com/98
    
    Signed-off-by: Youk Changsu <csyouk@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import json
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/<string:name>")
@@ -11,5 +17,11 @@ def index(name="Index"):
     }
     return render_template("index.html", **foo)
 
+@app.route("/db")
+def home_page():
+    online_users = mongo.db.users.find()
+    print(dict(online_users))
+    return json.dumps(dict(online_users))
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
