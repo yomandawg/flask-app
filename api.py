5049abcd41887c95d5eb98cde2c61ba17926b5a8
Youk Changsu <csyouk@gmail.com>
2020-03-16 23:42:51 +0900

    Fix routers.
    Since flask_restful only take charge in restful api,
    I divide it into two section.
    - restful api : only treat data processing
    - static file handler : return html files
    
    Signed-off-by: Youk Changsu <csyouk@gmail.com>

--- api.py
+++ api.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask import render_template
 from flask_restful import Api
-from handlers.handler import DefaultRouter, DBRouter
+from handlers.handler import DBRouter
 
 app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<string:name>')
+def get(name="hello"):
+    foo = {
+        "title": "Flask Example",
+        "bar": "Hello, World!",
+        "name": name
+    }
+    return render_template("index.html", **foo)
+
+
 api = Api(app)
-api.add_resource(DefaultRouter, '/', '/<string:name>')
 api.add_resource(DBRouter, '/db')
 
 if __name__ == "__main__":
--- handlers/handler.py
+++ handlers/handler.py
@@ -1,23 +1,8 @@
 from flask_restful import Resource
-from flask import render_template
 from pymongo import MongoClient
 import json
 
 client = MongoClient('localhost', 27017)
-
-
-class DefaultRouter(Resource):
-  def get(self):
-    return render_template("index.html")
-
-  def get(self, name):
-    foo = {
-      "title": "Flask Example",
-      "bar": "Hello, World!",
-      "name": name
-    }
-    return render_template("index.html", **foo)
-
 
 class DBRouter(Resource):
   def get(self):
