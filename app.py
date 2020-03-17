36f50b0f92a0744b3f4aff106e3b6d6436c72226
csyouk <csyouk@hanmail.net>
2020-03-17 22:54:42 +0900

    Add naver api example.
    To understand naver openapi usage,
    1. Sign-in : https://developers.naver.com/main/
    2. Register application. Refer :
    https://developers.naver.com/docs/datalab/shopping/#%EC%95%A0%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98-%EB%93%B1%EB%A1%9D-%EC%84%B8%EB%B6%80-%EC%A0%95%EB%B3%B4
    3. After register application, define app_config.py in your project.
    4. And, add two variables
      - cliend_id
      - client_secret
      above two variables defined in here : https://developers.naver.com/apps/#/list
    
    5. If you do all things, you can get results from naver openapi
      - ex) curl -X GET "http://localhost:5000/api/shopping"
    
    Signed-off-by: csyouk <csyouk@hanmail.net>

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask_restful import Api
-from handlers.handler import DBRouter
+from handlers.handler import DBRouter, ShoppingRouter
 
 #testing git-flow1
 
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/<string:name>')
@@ -20,7 +19,9 @@ def get(name="hello"):
 
 
 api = Api(app)
-api.add_resource(DBRouter, '/db')
+api.add_resource(DBRouter, '/api/db')
+api.add_resource(ShoppingRouter, '/api/shopping')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- handlers/handler.py
+++ handlers/handler.py
@@ -1,11 +1,46 @@
 from flask_restful import Resource
 from pymongo import MongoClient
+import requests
 import json
+
+try:
+    import app_config as cfg
+except NotImplementedError:
+    print("Please register app_config")
+    print("If you dont know how to register it,")
+    print("See : git log 40c1ad8093212ac47482181e40bdf9e1095a58c1")
+
+DATALAB_URL = "https://openapi.naver.com/v1/datalab/shopping/categories"
+QUERY = {
+    "startDate": "2017-08-01",
+    "endDate": "2017-09-30",
+    "timeUnit": "month",
+    "category": [
+        {"name": "패션의류", "param": ["50000000"]},
+        {"name": "화장품/미용", "param": ["50000002"]}
+    ],
+    "device": "pc",
+    "gender": "f",
+    "ages": ["20",  "30"]
+}
 
 client = MongoClient('localhost', 27017)
 
+headers = {
+    "X-Naver-Client-Id": cfg.client_id,
+    "X-Naver-Client-Secret": cfg.client_secret,
+    "Content-Type": "application/json"
+}
+
 class DBRouter(Resource):
-  def get(self):
-    online_users = client.db.users.find()
-    print(dict(online_users))
-    return json.dumps(dict(online_users))
+    def get(self):
+        online_users = client.db.users.find()
+        print(dict(online_users))
+        return json.dumps(dict(online_users))
+
+
+class ShoppingRouter(Resource):
+    def get(self):
+        r = requests.post(DATALAB_URL, data=json.dumps(QUERY), headers=headers)
+        print(r)
+        return r.json()
