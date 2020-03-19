d78aa1e53961f48d93a60a6fb1f1d5139018085a
csyouk <csyouk@hanmail.net>
2020-03-19 22:41:03 +0900

    Revert app.py for serving restful application.
    How to use
    - cd shoppingapp
    - flask run
    OR
    - export FLASK_APP=shpopingapp  (or set FLASKAPP in window)
    - flask run
    - Refer : https://stackoverflow.com/questions/29882642/how-to-run-a-flask-application
    
    Signed-off-by: csyouk <csyouk@hanmail.net>

--- shoppingapp/__init__.py
+++ shoppingapp/__init__.py
@@ -3,13 +3,13 @@ from flask_restful import Api
 
 
 def create_app():
-    app = Api(Flask(__name__))
+    app = Flask(__name__)
+
+    api = Api(app)
 
     from .handlers.handler import DBRouter, ShoppingRouter
 
-    app.add_resource(DBRouter, '/api/db')
-    app.add_resource(ShoppingRouter, '/api/shopping')
-
-    setattr(app, "testing", True)
+    api.add_resource(DBRouter, '/api/db')
+    api.add_resource(ShoppingRouter, '/api/shopping')
 
     return app
---
+++ shoppingapp/app.py
@@ -0,0 +1,5 @@
+from shoppingapp import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
